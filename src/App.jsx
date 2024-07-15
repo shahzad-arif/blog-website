41a489c03aac178f8382d456f2b02d8e7e17f6d0
chiragh12 <141055768+chiragh12@users.noreply.github.com>
2024-07-15 06:22:14 -0700

    Redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import authService from './appwrite/auth'
+import { useDispatch } from 'react-redux'
+import {login , logout} from './store/authSlice'
+import  Header  from './components/Header/Header.jsx'
+import Footer  from './components/Footer/Footer.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-    
-    <h1>A blog App</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+        
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>{
+      setLoading(false);
+    })
+  },[])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-slate-600'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+        Test{/* <Outlet/>*/} 
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
